a973497629b7db8afc87246ccc2bf9942aeeb5c7
programmer-man <bryan@kerigan.com>
2018-04-13 12:06:06 -0400

    made videos dynamic

--- wp-content/themes/kma-slim/template-parts/partials/home-video-gallery.php
+++ wp-content/themes/kma-slim/template-parts/partials/home-video-gallery.php
@@ -1,19 +1,25 @@
 <?php
+use Includes\Modules\Videos\Videos;
 
+$videoModule = new Videos();
+$mccarthyVideo = $videoModule->getVideos([], 'dr-kevin-mccarthy', 'video_author');
+$mccarthyVideo = $mccarthyVideo[0];
+$harrodVideo = $videoModule->getVideos([], 'dr-c-chambliss-harrod', 'video_author');
+$harrodVideo = $harrodVideo[0];
 ?>
 <div class="container has-text-centered" >
     <h2 class="title line-right line-left light" ><span class="line" ></span>&nbsp;Video Gallery&nbsp;<span class="line" ></span></h2>
     <p class="subtitle is-white">Our surgeons explain minimally invasive procedures.</p>
     <div class="columns is-justified" >
-        <div class="column is-6 has-text-right" @click="$emit('toggleModal', 'youtube', 'Jb2GQQdvjJ4')" >
+        <div class="column is-6 has-text-right" @click="$emit('toggleModal', '<?php echo $mccarthyVideo['video_type']; ?>', '<?php echo $mccarthyVideo['video_code']; ?>')" >
             <a class="image-link" >
-            <img class="video" src="<?php echo get_template_directory_uri() . '/img/mccarthy-video.jpg'; ?>">
+            <img class="video" src="<?php echo ($mccarthyVideo['photo'] == '' ? 'https://i.ytimg.com/vi/' . $mccarthyVideo['video_code'] . '/0.jpg' : $mccarthyVideo['photo']); ?>">
             </a>
             <a class="video-button button is-rounded is-primary is-glass" >PLAY&nbsp;<i class="fa fa-play" aria-hidden="true"></i></a></a>
         </div>
-        <div class="column is-6 has-text-right" @click="$emit('toggleModal', 'youtube', 'KwLT2NH0_Uk')" >
+        <div class="column is-6 has-text-right" @click="$emit('toggleModal', '<?php echo $harrodVideo['video_type']; ?>', '<?php echo $harrodVideo['video_code']; ?>')" >
             <a class="image-link" >
-                <img class="video" src="<?php echo get_template_directory_uri() . '/img/harrod-video.jpg'; ?>">
+                <img class="video" src="<?php echo ($harrodVideo['photo'] == '' ? 'https://i.ytimg.com/vi/' . $harrodVideo['video_code'] . '/0.jpg' : $harrodVideo['photo']); ?>">
             </a>
             <a class="video-button button is-rounded is-primary is-glass" >PLAY&nbsp;<i class="fa fa-play" aria-hidden="true"></i></a></a>
         </div>

829f3d9b3d989ad8436cbab543bfcd53edf15c7f
programmer-man <bryan@kerigan.com>
2018-04-13 16:07:15 -0400

    post titles/dates

--- wp-content/themes/kma-slim/template-parts/sidebars/patient-center-sidebar.php
+++ wp-content/themes/kma-slim/template-parts/sidebars/patient-center-sidebar.php
@@ -4,8 +4,8 @@ $pageList = get_children($parentId, [
     'post_parent'    => $parentId,
     'post_type'      => 'page',
     'posts_per_page' => -1,
-    'order'          => 'ASC',
     'orderby'        => 'menu_order',
+    'order'          => 'ASC',
 ]);
 ?>
 <div class="sidebar-module specialties-list">

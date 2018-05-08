ac581ad85a0e2d1f56d6b21b00c5a195654193c0
programmer-man <bryan@kerigan.com>
2018-05-08 11:01:16 -0400

    taga manager update

--- wp-content/themes/kma-slim/header.php
+++ wp-content/themes/kma-slim/header.php
@@ -16,14 +16,14 @@
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5X2845W');</script>
+        })(window,document,'script','dataLayer','GTM-MN4PCKB');</script>
     <!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> >
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5X2845W"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MN4PCKB"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', 'kmaslim'); ?></a>
